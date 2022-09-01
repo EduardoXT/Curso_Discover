3dd2fff2308fa285323e5c7530f1539640711f72
Eduardo Vinícius <72059808+EduardoXT@users.noreply.github.com>
2022-08-31 23:27:59 -0300

    adição de aulas

---
+++ Exercicios/Guia Estelar JavaScript/strings.js
@@ -0,0 +1,19 @@
+/* 
+
+Strings = Cadeia de caracteres 
+
+Exemplos:
+
+
+"" // Aspas duplas
+'' // Aspas simples
+`` // Template literals ou template strings
+
+Exemplos:
+*/
+
+console.log("Mayk'''") // Permite utilizar aspas simples dentro.
+
+console.log('"Ma"yk"') // Permite utilizar aspas duplas dentro.
+
+console.log(`Mayk ${1 + 1}`) // Permitem multilinhas e expressões de linguaguens.(Interpolações)
